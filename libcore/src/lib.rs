ed1815422d81fd1eba4f5b2f62ba79691200506a
Sayan Nandan <nandansayan@outlook.com>
2020-07-02 13:16:15 +0530

    Refactor project and add terrapipe spec

---
+++ libcore/src/lib.rs
@@ -0,0 +1,22 @@
+/*
+ * Created on Thu Jul 02 2020
+ *
+ * This file is a part of the source code for the Terrabase database
+ * Copyright (c) 2020 Sayan Nandan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+*/
+
+mod terrapipe;
