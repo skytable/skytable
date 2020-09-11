f8a1980898c810f4e3be82aa6f893544587adff7
Sayan Nandan <nandansayan@outlook.com>
2020-09-11 09:16:06 +0530

    Add tests for mget

--- server/src/tests/kvengine_tests.rs
+++ server/src/tests/kvengine_tests.rs
@@ -24,7 +24,6 @@ use crate::__func__;
 use tokio::prelude::*;
 
 #[tokio::test]
-#[cfg(test)]
 async fn test_queries() {
     // Start the server
     let (server, db) = start_server().await;
@@ -42,6 +41,11 @@ async fn test_queries() {
     queries.add(test_del_single_zero).await;
     queries.add(test_del_single_one).await;
     queries.add(test_del_multiple).await;
+    queries.add(test_del_syntax_error).await;
+    queries.add(test_mget_single_okay).await;
+    queries.add(test_mget_multiple_allokay).await;
+    queries.add(test_mget_multiple_mixed).await;
+    queries.add(test_mget_syntax_error).await;
     queries.run_queries_and_close_sockets();
 
     // Clean up everything else
@@ -49,7 +53,6 @@ async fn test_queries() {
     drop(db);
 }
 
-#[cfg(test)]
 /// Test a HEYA query: The server should return HEY!
 async fn test_heya(mut stream: TcpStream) -> TcpStream {
     let heya = terrapipe::proc_query("HEYA");
@@ -57,27 +60,20 @@ async fn test_heya(mut stream: TcpStream) -> TcpStream {
     let res_should_be = "#2\n*1\n#2\n&1\n+4\nHEY!\n".as_bytes().to_owned();
     let mut response = vec![0; res_should_be.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(response.to_vec(), res_should_be, "{}", __func__!());
+    assert_eq!(response, res_should_be, "{}", __func__!());
     stream
 }
 
-#[cfg(test)]
 /// Test a GET query: for a non-existing key
 async fn test_get_single_nil(mut stream: TcpStream) -> TcpStream {
     let get_single_nil = terrapipe::proc_query("GET x");
     stream.write_all(&get_single_nil).await.unwrap();
     let mut response = vec![0; fresp::R_NIL.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        response.to_vec(),
-        fresp::R_NIL.to_owned(),
-        "{}",
-        __func__!()
-    );
+    assert_eq!(response, fresp::R_NIL.to_owned(), "{}", __func__!());
     stream
 }
 
-#[cfg(test)]
 /// Test a GET query: for an existing key
 async fn test_get_single_okay(stream: TcpStream) -> TcpStream {
     let mut stream = test_set_single_okay(stream).await;
@@ -86,11 +82,10 @@ async fn test_get_single_okay(stream: TcpStream) -> TcpStream {
     let res_should_be = "#2\n*1\n#2\n&1\n+3\n100\n".as_bytes().to_owned();
     let mut response = vec![0; res_should_be.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(response.to_vec(), res_should_be, "{}", __func__!());
+    assert_eq!(response, res_should_be, "{}", __func__!());
     stream
 }
 
-#[cfg(test)]
 /// Test a GET query with an incorrect number of arguments
 async fn test_get_syntax_error(mut stream: TcpStream) -> TcpStream {
     let syntax_error = terrapipe::proc_query("GET");
@@ -98,7 +93,7 @@ async fn test_get_syntax_error(mut stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_ACTION_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_ACTION_ERR.to_owned(),
         "{}: With zero arg(s)",
         __func__!()
@@ -108,7 +103,7 @@ async fn test_get_syntax_error(mut stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_ACTION_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_ACTION_ERR.to_owned(),
         "{}: With two arg(s)",
         __func__!()
@@ -138,27 +133,20 @@ where
     let res_should_be = format!("#2\n*1\n#2\n&1\n:{}\n{}\n", count_bytes_len, homwany).into_bytes();
     let mut response = vec![0; res_should_be.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(response.to_vec(), res_should_be, "{}", __func__!());
+    assert_eq!(response, res_should_be, "{}", __func__!());
     stream
 }
 
-#[cfg(test)]
 /// Test a SET query: SET a non-existing key, which should return code: 0
 async fn test_set_single_okay(mut stream: TcpStream) -> TcpStream {
     let set_single_okay = terrapipe::proc_query("SET x 100");
     stream.write_all(&set_single_okay).await.unwrap();
     let mut response = vec![0; fresp::R_OKAY.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        response.to_vec(),
-        fresp::R_OKAY.to_owned(),
-        "{}",
-        __func__!()
-    );
+    assert_eq!(response, fresp::R_OKAY.to_owned(), "{}", __func__!());
     stream
 }
 
-#[cfg(test)]
 /// Test a SET query: SET an existing key, which should return code: 2
 async fn test_set_single_overwrite_error(stream: TcpStream) -> TcpStream {
     let mut stream = test_set_single_okay(stream).await;
@@ -167,7 +155,7 @@ async fn test_set_single_overwrite_error(stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_OVERWRITE_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_OVERWRITE_ERR.to_owned(),
         "{}",
         __func__!()
@@ -175,7 +163,6 @@ async fn test_set_single_overwrite_error(stream: TcpStream) -> TcpStream {
     stream
 }
 
-#[cfg(test)]
 /// Test a SET query with incorrect number of arugments
 async fn test_set_syntax_error(mut stream: TcpStream) -> TcpStream {
     let syntax_error = terrapipe::proc_query("SET");
@@ -183,7 +170,7 @@ async fn test_set_syntax_error(mut stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_ACTION_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_ACTION_ERR.to_owned(),
         "{}: With zero arg(s)",
         __func__!()
@@ -193,7 +180,7 @@ async fn test_set_syntax_error(mut stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_ACTION_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_ACTION_ERR.to_owned(),
         "{}: With one arg(s)",
         __func__!()
@@ -203,7 +190,7 @@ async fn test_set_syntax_error(mut stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_ACTION_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_ACTION_ERR.to_owned(),
         "{}: With four arg(s)",
         __func__!()
@@ -211,7 +198,6 @@ async fn test_set_syntax_error(mut stream: TcpStream) -> TcpStream {
     stream
 }
 
-#[cfg(test)]
 /// Test an UPDATE query: which should return code: 0
 async fn test_update_single_okay(stream: TcpStream) -> TcpStream {
     let mut stream = test_set_single_okay(stream).await;
@@ -219,16 +205,10 @@ async fn test_update_single_okay(stream: TcpStream) -> TcpStream {
     stream.write_all(&update_single_okay).await.unwrap();
     let mut response = vec![0; fresp::R_OKAY.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(
-        response.to_vec(),
-        fresp::R_OKAY.to_owned(),
-        "{}",
-        __func__!()
-    );
+    assert_eq!(response, fresp::R_OKAY.to_owned(), "{}", __func__!());
     stream
 }
 
-#[cfg(test)]
 /// Test an UPDATE query: which should return code: 1
 async fn test_update_single_nil(mut stream: TcpStream) -> TcpStream {
     let update_single_okay = terrapipe::proc_query("UPDATE x 200");
@@ -245,7 +225,7 @@ async fn test_update_syntax_error(mut stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_ACTION_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_ACTION_ERR.to_owned(),
         "{}: With zero arg(s)",
         __func__!()
@@ -255,7 +235,7 @@ async fn test_update_syntax_error(mut stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_ACTION_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_ACTION_ERR.to_owned(),
         "{}: With one arg(s)",
         __func__!()
@@ -265,7 +245,7 @@ async fn test_update_syntax_error(mut stream: TcpStream) -> TcpStream {
     let mut response = vec![0; fresp::R_ACTION_ERR.len()];
     stream.read_exact(&mut response).await.unwrap();
     assert_eq!(
-        response.to_vec(),
+        response,
         fresp::R_ACTION_ERR.to_owned(),
         "{}: With four arg(s)",
         __func__!()
@@ -273,7 +253,6 @@ async fn test_update_syntax_error(mut stream: TcpStream) -> TcpStream {
     stream
 }
 
-#[cfg(test)]
 /// Test a DEL query: which should return int 0
 async fn test_del_single_zero(mut stream: TcpStream) -> TcpStream {
     let update_single_okay = terrapipe::proc_query("DEL x");
@@ -281,11 +260,10 @@ async fn test_del_single_zero(mut stream: TcpStream) -> TcpStream {
     let res_should_be = "#2\n*1\n#2\n&1\n:1\n0\n".as_bytes().to_owned();
     let mut response = vec![0; res_should_be.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(response.to_vec(), res_should_be, "{}", __func__!());
+    assert_eq!(response, res_should_be, "{}", __func__!());
     stream
 }
 
-#[cfg(test)]
 /// Test a DEL query: which should return int 1
 async fn test_del_single_one(stream: TcpStream) -> TcpStream {
     let mut stream = set_values("x 100", 1, stream).await;
@@ -294,11 +272,10 @@ async fn test_del_single_one(stream: TcpStream) -> TcpStream {
     let res_should_be = "#2\n*1\n#2\n&1\n:1\n1\n".as_bytes().to_owned();
     let mut response = vec![0; res_should_be.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(response.to_vec(), res_should_be, "{}", __func__!());
+    assert_eq!(response, res_should_be, "{}", __func__!());
     stream
 }
 
-#[cfg(test)]
 /// Test a DEL query: which should return the number of keys deleted
 async fn test_del_multiple(stream: TcpStream) -> TcpStream {
     let mut stream = set_values("x 100 y 200 z 300", 3, stream).await;
@@ -307,6 +284,66 @@ async fn test_del_multiple(stream: TcpStream) -> TcpStream {
     let res_should_be = "#2\n*1\n#2\n&1\n:1\n3\n".as_bytes().to_owned();
     let mut response = vec![0; res_should_be.len()];
     stream.read_exact(&mut response).await.unwrap();
-    assert_eq!(response.to_vec(), res_should_be, "{}", __func__!());
+    assert_eq!(response, res_should_be, "{}", __func__!());
+    stream
+}
+
+/// Test a DEL query with an incorrect number of arguments
+async fn test_del_syntax_error(mut stream: TcpStream) -> TcpStream {
+    let syntax_error = terrapipe::proc_query("DEL");
+    stream.write_all(&syntax_error).await.unwrap();
+    let mut response = vec![0; fresp::R_ACTION_ERR.len()];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(response, fresp::R_ACTION_ERR.to_owned(), "{}", __func__!());
+    stream
+}
+
+/// Test an MGET query on a single existing key
+async fn test_mget_single_okay(stream: TcpStream) -> TcpStream {
+    let mut stream = set_values("x 100", 1, stream).await;
+    let query = terrapipe::proc_query("MGET x");
+    stream.write_all(&query).await.unwrap();
+    let res_should_be = "#2\n*1\n#2\n&1\n+3\n100\n".to_owned().into_bytes();
+    let mut response = vec![0; res_should_be.len()];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(response, res_should_be, "{}", __func__!());
+    stream
+}
+
+/// Test an MGET query on multiple existing keys
+async fn test_mget_multiple_allokay(stream: TcpStream) -> TcpStream {
+    let mut stream = set_values("x 100 y 200 z 300", 3, stream).await;
+    let query = terrapipe::proc_query("MGET x y z");
+    stream.write_all(&query).await.unwrap();
+    let res_should_be = "#2\n*1\n#2\n&3\n+3\n100\n+3\n200\n+3\n300\n"
+        .to_owned()
+        .into_bytes();
+    let mut response = vec![0; res_should_be.len()];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(response, res_should_be, "{}", __func__!());
+    stream
+}
+
+/// Test an MGET query with different outcomes
+async fn test_mget_multiple_mixed(stream: TcpStream) -> TcpStream {
+    let mut stream = set_values("x 100 z 200", 2, stream).await;
+    let query = terrapipe::proc_query("mget x y z");
+    stream.write_all(&query).await.unwrap();
+    let res_should_be = "#2\n*1\n#2\n&3\n+3\n100\n!1\n1\n+3\n200\n"
+        .to_owned()
+        .into_bytes();
+    let mut response = vec![0; res_should_be.len()];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(response, res_should_be);
+    stream
+}
+
+/// Test an MGET query with an incorrect number of arguments
+async fn test_mget_syntax_error(mut stream: TcpStream) -> TcpStream {
+    let syntax_error = terrapipe::proc_query("MGET");
+    stream.write_all(&syntax_error).await.unwrap();
+    let mut response = vec![0; fresp::R_ACTION_ERR.len()];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(response, fresp::R_ACTION_ERR.to_owned());
     stream
 }
