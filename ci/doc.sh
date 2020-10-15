2a9fa9ffcf28af2daa455a575d5f285aad75e886
Sayan Nandan <nandansayan@outlook.com>
2020-10-15 17:39:28 +0530

    Fix script to skip builds iff md files are changed In previous commits, we ignored the fact that bools are strings in bash This caused the workflow to proceed wrongly and this commit fixes that Also, auto builds for actions docs were added

---
+++ ci/doc.sh
@@ -0,0 +1,13 @@
+{
+    git config --global user.email "$BOT_MAIL"
+    git config --global user.name "$BOT_USER"
+    C_USER=$(git show -s --format='%an' HEAD)
+    C_MAIL=$(git show -s --format='%ae' HEAD)
+    cd ..
+    git clone https://github.com/terrabasedb/docs.git
+    cd docs
+    chmod +x builddoc.sh
+    sh ./builddoc.sh
+    git add . && git commit -m "Updated docs from upstream" -m "Triggered by ${GITHUB_SHA}" --author "$C_USER <$C_MAIL>"
+    eval '${BOT_API}'
+} >>/dev/null
