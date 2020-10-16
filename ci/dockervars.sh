07f97c65b3e02fd16e97cb7c93d5a953447f0649
Sayan Nandan <nandansayan@outlook.com>
2020-10-16 12:45:31 +0530

    Optimize CI script

--- ci/dockervars.sh
+++ ci/dockervars.sh
@@ -1,9 +1,10 @@
-set -euo pipefail
-RS_CHANGED_COUNT=$(git diff --numstat HEAD^..HEAD -- '*.rs' | wc -l)
-DOCKERFILE_CHANGED=$(git diff --numstat HEAD^..HEAD -- 'Dockerfile' | wc -l)
-DOCKER_CI_CHANGED=$(git diff --numstat HEAD^..HEAD -- 'docker-image.yml' | wc -l)
+# This script checks if any source files were modified in a push event
+# If source files were indeed modified, then this script sets `BUILD=true`
 
-if [ '$RS_CHANGED_COUNT' != "0" ] || [ '$DOCKERFILE_CHANGED' != "0" ] || [ '$DOCKER_CI_CHANGED' != "0" ]; then
+set -euo pipefail
+SRC_CHANGED_COUNT=$(git diff --numstat HEAD^..HEAD -- '*.rs' '*.yml' '*.toml' 'Dockerfile' | wc -l)
+
+if [ '$SRC_CHANGED_COUNT' != "0" ]; then
     echo "The docker image has to be built"
     echo "BUILD=true" >>$GITHUB_ENV
 fi
