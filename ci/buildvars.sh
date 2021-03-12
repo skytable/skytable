5392f27fdd0dceb7a1e679c6f166bdb60c371008
Sayan Nandan <nandansayan@outlook.com>
2021-03-12 13:10:40 +0530

    Improve builds and fix release builds (#105)
    * Checkout the PR branch if triggered by a PR
    
    Signed-off-by: Sayan Nandan <nandansayan@outlook.com>
    
    * Use `cmd` as the shell on Windows to fix builds
    
    Signed-off-by: Sayan Nandan <nandansayan@outlook.com>
    
    #104 did this for pushes; this will do it for releases.
    
    * Fix Windows release script
    
    Signed-off-by: Sayan Nandan <nandansayan@outlook.com>
    
    * No need to checkout to head.ref for PRs
    
    Signed-off-by: Sayan Nandan <nandansayan@outlook.com>

--- ci/buildvars.sh
+++ ci/buildvars.sh
@@ -2,6 +2,7 @@
 # If source files were indeed modified, then this script sets `BUILD=true`
 
 set -euo pipefail
+
 SRC_CHANGED_COUNT=$(git diff --numstat HEAD^..HEAD -- '*.rs' '*.yml' '*.toml' 'Dockerfile' | wc -l)
 
 if [ $SRC_CHANGED_COUNT != "0" ]; then
