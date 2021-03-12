fd487affe9ada4bddfdcf398d3b6295af802a42d
Sayan Nandan <nandansayan@outlook.com>
2021-03-12 12:39:55 +0530

    Checkout the PR branch if triggered by a PR
    Signed-off-by: Sayan Nandan <nandansayan@outlook.com>

--- ci/buildvars.sh
+++ ci/buildvars.sh
@@ -2,6 +2,12 @@
 # If source files were indeed modified, then this script sets `BUILD=true`
 
 set -euo pipefail
+
+if [ $PR == "true" ]; then
+    # This is a pull request, so compare using the $PR_BRANCH
+    git checkout $PR_BRANCH
+fi
+
 SRC_CHANGED_COUNT=$(git diff --numstat HEAD^..HEAD -- '*.rs' '*.yml' '*.toml' 'Dockerfile' | wc -l)
 
 if [ $SRC_CHANGED_COUNT != "0" ]; then
