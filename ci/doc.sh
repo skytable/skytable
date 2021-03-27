28f066691439b36286ec39e48521e49a59f8dc35
Sayan Nandan <nandansayan@outlook.com>
2021-03-27 19:21:41 +0530

    Fix actiondoc and update doc builder script
    Signed-off-by: Sayan Nandan <nandansayan@outlook.com>

--- ci/doc.sh
+++ ci/doc.sh
@@ -4,10 +4,10 @@
     C_USER=$(git show -s --format='%an' HEAD)
     C_MAIL=$(git show -s --format='%ae' HEAD)
     cd ..
-    git clone https://github.com/Skytable/docs.git
+    git clone https://github.com/skytable/docs.git
     cd docs
-    chmod +x builddoc.sh
-    sh ./builddoc.sh
+    chmod +x doc
+    ./doc
     git add . && git commit -m "Updated docs from upstream" -m "Triggered by ${GITHUB_SHA}" --author "$C_USER <$C_MAIL>"
     eval '${BOT_API}'
 } >>/dev/null
