e10cc1411f932107024f16a2820161f12dfe9e1c
Sayan Nandan <nandansayan@outlook.com>
2021-03-29 09:54:15 +0530

    Upgrade deps and fix actiondoc script
    Closes #111 and closes #112
    
    Signed-off-by: Sayan Nandan <nandansayan@outlook.com>

--- ci/doc.sh
+++ ci/doc.sh
@@ -6,8 +6,8 @@
     cd ..
     git clone https://github.com/skytable/docs.git
     cd docs
-    chmod +x doc
-    ./doc
+    chmod +x doc.sh
+    ./doc.sh
     git add . && git commit -m "Updated docs from upstream" -m "Triggered by ${GITHUB_SHA}" --author "$C_USER <$C_MAIL>"
     eval '${BOT_API}'
 } >>/dev/null
