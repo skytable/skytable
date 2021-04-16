83911af208d5ab72282f71f885d18baaa6be573a
Sayan Nandan <nandansayan@outlook.com>
2021-04-16 16:10:04 +0000

    Only run build.rs for fscposix.c on unix systems

--- server/build.rs
+++ server/build.rs
@@ -1,6 +1,8 @@
+#[cfg(unix)]
 use cc;
 fn main() {
+    #[cfg(unix)]
     cc::Build::new()
-    .file("native/fscposix.c")
-    .compile("libflock.a")
+        .file("native/fscposix.c")
+        .compile("libflock-posix.a")
 }
