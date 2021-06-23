cbf0ac6523fc7c5ac36aa7ee58dbee0ab1e074e2
Sayan Nandan <nandansayan@outlook.com>
2021-06-23 10:59:47 +0530

    Use caching on ARM runners and remove is_md_file
    Since GH now supports `skip ci`, we don't need this anymore

--- ci/setvars.sh
+++ ci/setvars.sh
@@ -6,12 +6,7 @@
 set -euo pipefail
 FILES_CHANGED=$(git diff --numstat HEAD^..HEAD | wc -l)
 ACTIONS_CHANGED_COUNT=$(git diff --numstat HEAD^..HEAD -- 'actions.jsonc' | wc -l)
-MD_CHANGED_COUNT=$(git diff --numstat HEAD^..HEAD -- '*.md' | wc -l)
-if [ ${FILES_CHANGED} = ${MD_CHANGED_COUNT} ]; then
-    # This push just modifies markdown files
-    echo "This push only modifies markdown files"
-    echo "IS_MD_FILE=true" >>$GITHUB_ENV
-elif [ ${ACTIONS_CHANGED_COUNT} = "1" ]; then
+if [ ${ACTIONS_CHANGED_COUNT} = "1" ]; then
     # This push changes the actions documentation
     echo "This push modifies the actions.jsonc file"
     echo "IS_ACTIONS_DOC=true" >>$GITHUB_ENV
