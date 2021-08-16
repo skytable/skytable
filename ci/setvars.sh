18e8f74f0aa28a55c71c8d2c002f4c12a4a02413
Sayan Nandan <nandansayan@outlook.com>
2021-08-16 00:15:07 -0700

    Add new actiondoc
    Also moved actiondoc rebuild to separate workflow

--- ci/setvars.sh
+++ ci/setvars.sh
@@ -1,13 +1,13 @@
 # This script sets a couple of environment variables
 # If *.md files are modified, then there is no need for running the expensive
 # build steps, instead we just set the `IS_MD_FILE` variable to true.
-# Similarly, if the `actions.jsonc` file is modified, the documentation needs to be
+# Similarly, if the `actiondoc.yml` file is modified, the documentation needs to be
 # be updated
 set -euo pipefail
 FILES_CHANGED=$(git diff --numstat HEAD^..HEAD | wc -l)
-ACTIONS_CHANGED_COUNT=$(git diff --numstat HEAD^..HEAD -- 'actions.jsonc' | wc -l)
+ACTIONS_CHANGED_COUNT=$(git diff --numstat HEAD^..HEAD -- 'actiondoc.yml' | wc -l)
 if [ ${ACTIONS_CHANGED_COUNT} = "1" ]; then
     # This push changes the actions documentation
-    echo "This push modifies the actions.jsonc file"
+    echo "This push modifies the actiondoc.yml file"
     echo "IS_ACTIONS_DOC=true" >>$GITHUB_ENV
 fi
