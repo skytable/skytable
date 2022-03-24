d306307fd88b6ab9db9c773f4962eb4413272a74
Sayan Nandan <nandansayan@outlook.com>
2022-03-23 21:42:06 -0700

    Fix doc workflow not reporting errors and `auth whoami` actiondoc

--- ci/doc.sh
+++ ci/doc.sh
@@ -1,13 +1,13 @@
+git config --global user.email "$BOT_MAIL"
+git config --global user.name "$BOT_USER"
+C_USER=$(git show -s --format='%an' HEAD)
+C_MAIL=$(git show -s --format='%ae' HEAD)
+cd ..
+git clone https://github.com/skytable/docs.git
+cd docs
+chmod +x doc.sh
+./doc.sh
 {
-    git config --global user.email "$BOT_MAIL"
-    git config --global user.name "$BOT_USER"
-    C_USER=$(git show -s --format='%an' HEAD)
-    C_MAIL=$(git show -s --format='%ae' HEAD)
-    cd ..
-    git clone https://github.com/skytable/docs.git
-    cd docs
-    chmod +x doc.sh
-    ./doc.sh
     git add . && git commit -m "Updated docs from upstream" -m "Triggered by ${GITHUB_SHA}" --author "$C_USER <$C_MAIL>"
     eval '${BOT_API}'
 } >>/dev/null
