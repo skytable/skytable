38884f994295797d98a647d8af43d97cd10e5b87
Sayan Nandan <nandansayan@outlook.com>
2025-01-07 11:35:21 -0800

    server [config]: Add preliminary impls for v2

--- server/src/engine/config/v2.rs
+++ server/src/engine/config/v2.rs
@@ -22,3 +22,90 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  *
 */
+
+#![allow(dead_code)]
+
+use std::{
+    collections::HashMap,
+    env::{self, VarError},
+    ops,
+};
+
+type ConfigResult<T> = Result<T, ConfigError>;
+
+/*
+    config item
+*/
+
+/// a configuration group
+pub trait ConfigGroup: Sized {
+    /// parse this configuration group using the provided CLI args
+    fn from_cli(args: &mut HashMap<String, String>) -> ConfigResult<ConfigReturn<Self>>;
+    /// parse this configuration group using env vars
+    fn from_env() -> ConfigResult<ConfigReturn<Self>>;
+    /// parse this configuration group using the provided vars
+    fn from_env_test(args: &mut HashMap<String, String>) -> ConfigResult<ConfigReturn<Self>>;
+}
+
+/// similar to [`ConfigGroup`], this trait is to be used when a config group is more complex to decode
+/// and needs to access, for example, multiple variables
+pub trait ConfigGroupOverride: Sized {
+    /// parse from cli args
+    fn from_cli(
+        args: &mut HashMap<String, String>,
+        cpath: &str,
+    ) -> ConfigResult<ConfigReturn<Self>>;
+    /// parse from env vars
+    fn from_env(cpath: &str) -> ConfigResult<ConfigReturn<Self>>;
+    /// parse from provided vars
+    fn from_env_test(
+        args: &mut HashMap<String, String>,
+        cpath: &str,
+    ) -> ConfigResult<ConfigReturn<Self>>;
+}
+
+/*
+    errors
+*/
+
+#[derive(Debug, PartialEq)]
+/// errors resulting from parsing and evaluating configuration options
+pub enum ConfigError {
+    /// the configuration item was required but is missing
+    Required(String),
+    /// failed to parse the configuration item
+    ParseError(String),
+}
+
+/*
+    utils
+*/
+
+fn get_var(v: &str) -> ConfigResult<Option<String>> {
+    Ok(match env::var(v) {
+        Ok(v) => Some(v),
+        Err(VarError::NotPresent) => None,
+        Err(VarError::NotUnicode(e)) => {
+            return Err(ConfigError::ParseError(format!(
+                "failed to parse value for env var `{v}` - {e}",
+                e = e.to_string_lossy()
+            )))
+        }
+    })
+}
+
+#[derive(Debug, PartialEq)]
+/// Modified or unmodified [`ConfigItem`]
+pub enum ConfigReturn<T> {
+    Modified(T),
+    Unmodified(T),
+}
+
+impl<T> ops::Deref for ConfigReturn<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Self::Modified(m) | Self::Unmodified(m) => m,
+        }
+    }
+}
